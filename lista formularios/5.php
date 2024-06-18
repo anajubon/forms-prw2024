7b417e58942d98ef6e12bf21e75ea9de980d1c45
bonfieti <bonfietianajulia@gmail.com>
2024-06-18 16:29:29 -0300

    5

--- lista formularios/5.php
+++ lista formularios/5.php
@@ -8,20 +8,15 @@
 </head>
 <body>
 <?php
-    // Verifica se os valores foram enviados pelo formulário
-    if (isset($_POST['co']) && isset($_POST['la'])) {
-        // Recebe os valores do formulário
-        $comprimento = $_POST['co'];
-        $largura = $_POST['la'];
+    // Recebe os valores do formulário
+    $comprimento = $_POST['co'];
+    $largura = $_POST['la'];
 
-        // Calcula a área do terreno
-        $a = $comprimento * $largura;
+    // Calcula a área do terreno
+    $a = $comprimento * $largura;
 
-        // Exibe a área do terreno
-        echo "<p>A área do terreno é: " . $a . " metros quadrados.</p>";
-    } else {
-        echo "<p>Por favor, preencha todos os campos do formulário.</p>";
-    }
+    // Exibe a área do terreno
+    echo "<p>A área do terreno é: " . $a . " metros quadrados.</p>";
     ?>
 </body>
 </html>
